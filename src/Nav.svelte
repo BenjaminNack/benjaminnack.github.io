33a6435f2bbe06f1537850c8dbba6164753558eb
Benjamin Nack <ben@nackfamily.com>
2020-11-30 16:51:39 -0600

    Add nav bar

---
+++ src/Nav.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Index from './pages/Index.svelte';
+
+    let page = 0;
+</script>
+
+<nav>
+    <ul class="page-list">
+        <li class="page-item" on:click={() => page = 0}>Portfolio</li>
+    </ul>
+</nav>
+
+{#if page === 0}
+<Index/>
+{/if}
+
+<style>
+    .page-list {
+        list-style-type: none;
+    }
+
+    .page-item {
+        user-select: none;
+        -webkit-touch-callout: none;
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
+
+    .page-item:hover {
+        color: #8baaff;
+    }
+</style>
