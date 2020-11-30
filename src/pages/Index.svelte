6ed3132e6716192235751e1885d330a717a45556
Benjamin Nack <ben@nackfamily.com>
2020-11-30 16:52:22 -0600

    Added header and styling Added cards and styling

---
+++ src/pages/Index.svelte
@@ -0,0 +1,52 @@
+<script>
+
+</script>
+
+<header>
+    <section class="header-tags">
+        <h1>Hi,</h1>
+        <h1>my name is Benjamin,</h1>
+        <h1>but you can call me Ben</h1>
+        <h3>I'm a self led software developer/engineer.</h3>
+    </section>
+</header>
+
+<div class="projects">
+    <div class="card">
+
+    </div>
+</div>
+
+<style>
+    header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+        flex-direction: column;
+        width: 100%;
+    }
+
+    header h1 {
+        margin-top: 0;
+        margin-bottom: 3vh;
+    }
+
+    header h3 {
+        margin-top: 10vh;
+    }
+
+    .projects {
+        display: grid -moz-grid;
+        grid-template-columns: auto;
+        height: 300px;
+    }
+
+    .card {
+        background-color: white;
+        height: 100%;
+        width: 300px;
+        border-radius: 5%;
+        box-shadow: 4px 4px 4px black;
+    }
+</style>
